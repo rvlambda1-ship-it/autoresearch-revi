382facec0f5526cd10942433ed0b6c8922a0c337
Revi Siva <rvlambda@gmail.com>
2026-03-17 16:23:01 +0530

    experiment: remove logit softcap entirely

--- train.py
+++ train.py
@@ -287,10 +287,8 @@ class GPT(nn.Module):
             x = block(x, ve, cos_sin, self.window_sizes[i])
         x = norm(x)
 
-        softcap = 30
         logits = self.lm_head(x)
         logits = logits.float()
-        logits = softcap * torch.tanh(logits / softcap)
 
         if targets is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1),
