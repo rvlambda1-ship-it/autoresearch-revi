0de55664d52482017a280b4b53a75b3b60c86fa1
Revi Siva <rvlambda@gmail.com>
2026-03-24 04:37:46 +0530

    experiment: MLP ratio 2.5x (further reducing MLP compute)

--- train.py
+++ train.py
@@ -103,8 +103,9 @@ class CausalSelfAttention(nn.Module):
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.c_fc = nn.Linear(config.n_embd, 3 * config.n_embd, bias=False)
-        self.c_proj = nn.Linear(3 * config.n_embd, config.n_embd, bias=False)
+        hidden = (5 * config.n_embd) // 2  # 2.5x MLP ratio
+        self.c_fc = nn.Linear(config.n_embd, hidden, bias=False)
+        self.c_proj = nn.Linear(hidden, config.n_embd, bias=False)
 
     def forward(self, x):
         x = self.c_fc(x)
