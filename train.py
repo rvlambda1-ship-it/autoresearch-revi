df83e7f7501891b99fde42084c4406cf5963ac2c
Revi Siva <rvlambda@gmail.com>
2026-03-24 17:48:24 +0530

    discard: lm_head init std 0.002 worse (2.244 vs 2.225)

--- train.py
+++ train.py
@@ -159,7 +159,7 @@ class GPT(nn.Module):
     def init_weights(self):
         # Embedding and unembedding
         torch.nn.init.normal_(self.transformer.wte.weight, mean=0.0, std=1.0)
-        torch.nn.init.normal_(self.lm_head.weight, mean=0.0, std=0.002)
+        torch.nn.init.normal_(self.lm_head.weight, mean=0.0, std=0.001)
         # Transformer blocks
         n_embd = self.config.n_embd
         s = 3**0.5 * n_embd**-0.5
