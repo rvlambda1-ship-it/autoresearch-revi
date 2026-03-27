a2ccc31872bae01424a4d8b87ee08baf1eb480be
Revi Siva <rvlambda@gmail.com>
2026-03-27 06:12:15 +0530

    experiment: Muon ns_steps=2 at batch 2K

--- train.py
+++ train.py
@@ -269,7 +269,7 @@ class GPT(nn.Module):
             if USE_MUON:
                 param_groups.append(dict(
                     kind='muon', params=group_params, lr=matrix_lr,
-                    momentum=0.95, ns_steps=3, beta2=0.95, weight_decay=weight_decay,
+                    momentum=0.95, ns_steps=2, beta2=0.95, weight_decay=weight_decay,
                 ))
             else:
                 param_groups.append(dict(
